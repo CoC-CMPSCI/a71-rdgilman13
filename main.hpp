1e0a5ef4c6a5c183de6d0241791340ad517ec1f8
Rollin Gilman <rdgilman@my.canyons.edu>
2026-05-10 13:09:41 -0700

    a71 Bubble Sort

--- main.hpp
+++ main.hpp
@@ -11,10 +11,34 @@ const int SIZE = 100;
 
 int makearray(int number[])
 {
+    int last = rand() % 11 + 10;   // random size 10-20
+
+    for (int i = 0; i < last; i++)
+    {
+        number[i] = rand() % 100;  // random value 0-99
+    }
+
+    return last;
 }
+
 void bubble(int number[], int last)
 {
+    for (int i = 0; i < last - 1; i++)
+    {
+        if (number[i] > number[i + 1])
+        {
+            int temp = number[i];
+            number[i] = number[i + 1];
+            number[i + 1] = temp;
+        }
+    }
 }
+
 void printout(int number[], int last)
 {
+    for (int i = 0; i < last; i++)
+    {
+        cout << number[i] << "\t";
+    }
+    cout << endl;
 }
